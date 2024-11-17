cb3315ae24701878bd0a8997d38dc7ba4da6f5bc
Unknownkiller7777 <126313471+Unknownkil@users.noreply.github.com>
2024-11-17 13:58:18 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from telebot import types
 import subprocess
 
 # Telegram bot token
-TOKEN = '7610132838:AAFCZHxS96lqIejFQAfjcRjnjhm-GHMPZb0'
+TOKEN = 'BOT_TOKEN'
 bot = telebot.TeleBot(TOKEN)
 
 print(" Bot start ho gya GuRu ")
